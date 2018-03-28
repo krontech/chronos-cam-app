2ac2e813a74a4c7b2ea9933eb04ee1d89a60a6e5
David Roberts <robertsdavidddr@gmail.com>
2018-03-27 17:03:40 -0700

    Removed reference to /dev/sda1.
    It won't mean anything to anybody in that context, I don't think. What was the intended use case?
    
    Also, we should probably back up to SD cards as well, use whatever's plugged in. But that can come later. 🤔

--- src/utilwindow.cpp
+++ src/utilwindow.cpp
@@ -525,7 +525,7 @@ void UtilWindow::on_cmdSetSN_clicked()
 
 void UtilWindow::statErrorMessage(){
 	QMessageBox msg;
-	msg.setText("Error: USB device (/media/sda1) not present");
+	msg.setText("Error: USB device not detected"); // /media/sda1
 	msg.setWindowFlags(Qt::WindowStaysOnTopHint);
 	msg.exec();
 }
