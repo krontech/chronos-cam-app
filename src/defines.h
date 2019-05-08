ecb58aa306f5395a762a6cc4778149bb459d0130
Owen Kirby <oskirby@gmail.com>
2019-05-08 16:30:47 -0700

    Update version to 0.4.0-alpha

--- src/defines.h
+++ src/defines.h
@@ -50,7 +50,7 @@
 #define SERIAL_NUMBER_OFFSET	0
 #define SERIAL_NUMBER_MAX_LEN	32		//Maximum number of characters in serial number
 
-#define CAMERA_APP_VERSION		"0.3.1"
+#define CAMERA_APP_VERSION		"0.4.0-alpha"
 #define ACCEPTABLE_FPGA_VERSION	3
 
 #define FLAG_TEMPORARY  1
