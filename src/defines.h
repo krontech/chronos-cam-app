fdb27f09d944f18f9d287a29dcfba5d076b1c261
Simon Kronstein <skronstein@gmail.com>
2019-07-23 16:08:21 -0700

    v0.3.2-RC1

--- src/defines.h
+++ src/defines.h
@@ -50,7 +50,7 @@
 #define SERIAL_NUMBER_OFFSET	0
 #define SERIAL_NUMBER_MAX_LEN	32		//Maximum number of characters in serial number
 
-#define CAMERA_APP_VERSION		"0.3.2-beta"
+#define CAMERA_APP_VERSION		"0.3.2-RC1"
 #define ACCEPTABLE_FPGA_VERSION	3
 
 #define FLAG_TEMPORARY  1
