60103b82c5515bd4a18429053365ed9b8adceac7
Owen Kirby <oskirby@gmail.com>
2019-07-25 18:33:12 -0700

    Make version.sh work for out-of-tree builds.
    version.sh was assuming to be invoked from a child directory of
    the source git repository, but this is not the case for out-of-tree
    builds.

--- src/version.sh
+++ src/version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-VERS=$(git describe --always --tags --dirty)
+SRCDIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+VERS=$(cd ${SRCDIR} && git describe --always --tags --dirty)
 cat << EOF
 const char *git_version_str = "${VERS}";
 EOF
