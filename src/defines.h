5b1b176b0c5194ebb4f62cb53230b2fa8bf7c79c
sanjpm <smaharaj@krontech.ca>
2021-02-02 17:29:27 -0800

    Incremented minor build number.
    Incremented from 0.5.1 -> 0.6.0 to signify a new minor build candidate.

--- src/defines.h
+++ src/defines.h
@@ -25,7 +25,7 @@
 #define SERIAL_NUMBER_OFFSET	0
 #define SERIAL_NUMBER_MAX_LEN	32		//Maximum number of characters in serial number
 
-#define CAMERA_APP_VERSION		"0.5.1"
+#define CAMERA_APP_VERSION		"0.6.0"
 #define ACCEPTABLE_FPGA_VERSION	3
 
 // Network Storage Paths
